724fc7fa92715411a86e37adc204b350cea34f41
PrerakMathur20 <mathur.prerak@gmail.com>
2026-01-16 14:17:37 +0530

    colors.ts and related files

---
+++ packages/tokens/src/primitives/motion.ts
@@ -0,0 +1,10 @@
+export const motion = {
+  duration: {
+    fast: '150ms',
+    normal: '300ms',
+    slow: '500ms',
+  },
+  easing: {
+    easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+} as const;
